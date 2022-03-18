c68334b197989eac5da282f2f6b71fa279898be9
springus <90499150+springus@users.noreply.github.com>
2022-03-18 19:15:26 +0900

    add mint ostream

--- mint.cpp
+++ mint.cpp
@@ -1,29 +1,29 @@
 const long long mod=1e9+7;
-//const long long mod=998244353;
-struct mint{
-  long long x;
-  mint(long long x=0) : x((x%mod+mod)%mod){ }
+//const long long mod= ;
+class mint{
+private:long long x;
+public:
+  mint(long long _x=0) : x((_x%mod+mod)%mod){ }
   mint operator -( )const{return mint(-x);}
-  mint& operator +=(const mint &a) {
-    if((x+=a.x)>=mod)x-=mod;
+  mint& operator +=(const mint &rhs) {
+    if((this->x+=rhs.x)>=mod)x-=mod;
     return *this;
   }
-  mint& operator -=(const mint &a){
-    if((x+=mod-a.x)>=mod)x-=mod;
+  mint& operator -=(const mint &rhs){
+    if((this->x+=mod-rhs.x)>=mod)x-=mod;
     return *this;
   }
-  mint& operator *=(const mint &a){
-    (x*=a.x)%=mod;
+  mint& operator *=(const mint &rhs){
+    (this->x*=rhs.x)%=mod;
     return *this;
   }
-  mint& operator /=(const mint &a){
-    return (*this)*=a.inv();
+  mint& operator /=(const mint &rhs){
+    return (*this)*=rhs.inv();
   }
-  mint operator+(const mint &a) const{return mint(*this)+=a;}
-  mint operator-(const mint &a) const{return mint(*this)-=a;}
-  mint operator*(const mint &a) const{return mint(*this)*=a;}
-  mint operator/(const mint &a) const{return mint(*this)/=a;}
-
+  mint operator+(const mint &rhs) const{return mint(*this)+=rhs;}
+  mint operator-(const mint &rhs) const{return mint(*this)-=rhs;}
+  mint operator*(const mint &rhs) const{return mint(*this)*=rhs;}
+  mint operator/(const mint &rhs) const{return mint(*this)/=rhs;}
   mint pow(long long t)const{
     if(t<1)return 1;
     mint a=pow(t>>1);
@@ -32,4 +32,5 @@ struct mint{
     return a;
   }
   mint inv() const{return pow(mod-2);}
+  friend ostream &operator<<(ostream &os ,const mint &a){return os<<a.x;}
 };
